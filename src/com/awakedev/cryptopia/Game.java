54b7a040e79b4b7dd2a8f186d4044ea40e74e307
Nathan Corry <32592701+Wander1ust@users.noreply.github.com>
2019-01-03 20:54:11 +0000

    implementing canvas

--- src/com/awakedev/cryptopia/Game.java
+++ src/com/awakedev/cryptopia/Game.java
@@ -1,5 +1,86 @@
 package com.awakedev.cryptopia;
 
-public class Game {
+// Imports
+ 
+import java.awt.Canvas;
+import java.awt.Dimension;
 
+import javax.swing.JFrame;
+
+// Game inherits Canvas (subclass of Canvas)
+
+public class Game extends Canvas implements Runnable {
+	
+	// Creating window and defining resolutions & scale.
+	
+	public static int width = 300;
+	public static int height = width / 16 * 9;
+	public static int scale = 3;
+	
+	
+	private Thread thread;
+	private JFrame frame;
+	private boolean running = false;
+	
+	
+	// Game constructor 
+	
+	public Game () {
+		Dimension size = new Dimension(width*scale, height*scale);
+		setPreferredSize(size);
+		
+		frame = new JFrame();
+	}
+	
+	
+
+	public synchronized void start() {
+		running = true;
+		
+		thread = new Thread(this, "Display");
+		thread.start();
+	}
+	
+
+	public synchronized void stop() {
+		running = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	// Game loop
+	
+	public void run() {
+		while (running = true) {
+			
+		}	
+	} 
+	
+	// Main method (Entry point)
+	
+	public static void main (String[] args) {
+		
+		// Object of game with instance
+		
+		Game game = new Game();
+		game.frame.setResizable(false);
+		game.frame.setTitle("Cryptopia");
+		
+		// Adding game component to window
+		game.frame.add(game);
+		
+		game.frame.pack();
+		
+		// End application when exit button clicked
+		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	
+	
+	
+	
 }
